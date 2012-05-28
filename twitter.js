b11e4a5709cdced0a2e380bc60c3c581e918a517
Francis Gulotta <wizard@roborooter.com>
2012-05-27 20:33:25 -0400

    Add a twitter collection for datasource example

---
+++ twitter.js
@@ -0,0 +1,24 @@
+var TweetSearch = BTS.SortableList.extend({
+	
+	url: function(){
+		if(!this.search){throw new Error('No search defined');}
+		return 'http://search.twitter.com/search.json?q=' + this.search;
+	},
+
+	initialize: function(models, opt){
+		opt = opt || {};
+		this.search = opt.search;
+		BTS.SortableList.prototype.initialize.call(this);
+	},
+
+	sync: function(method, model, opt){
+		opt = opt || {};
+		opt.dataType = 'jsonp';
+		return Backbone.sync.call(this, method, model, opt);
+	},
+
+	parse: function(res){
+		return res.results;
+	}
+
+});
